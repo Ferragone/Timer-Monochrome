54929fa7cbb3e6ff7d8be0ec386240fd6672a08c
Artem Tykhonenko <128739984+Ferragone@users.noreply.github.com>
2023-07-07 22:24:44 +0300

    Js countdown

--- app.js
+++ app.js
@@ -5,11 +5,51 @@ class App {
     sec_tens: document.getElementById("sec_tens"),
     sec: document.getElementById("sec"),
   };
+  #interval;
 
   submit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const time = formData.get("time");
+    this.#clearTimer();
+    this.#startTimer(time);
+  }
+
+  #clearTimer() {
+    if (this.#interval) {
+      clearInterval(this.#interval);
+    }
+    this.#startTimer({
+      min_tens: 0,
+      min: 0,
+      sec_tens: 0,
+      sec: 0,
+    });
+  }
+
+  #startTimer(time) {
+    const end = Date.now() + time * 1000 * 60;
+    this.#interval = setInterval(() => {
+      const now = Date.now();
+      const delta = end - now;
+      if (delta < 0) {
+        clearInterval(this.#interval);
+        return;
+      }
+      this.#startTimer({
+        min_tens: Math.floor(delta / 1000 / 60 / 10),
+        min: ath.floor((delta / 1000 / 60) % 10),
+        sec_tens: Math.floor((delta % 60000) / 10000),
+        sec: Math.floor(((delta % 60000) / 10000) % 10),
+      });
+    }, 500);
+  }
+
+  #setTimer(min_tens, min, sec_tens, sec) {
+    this.timer.min_tens.innerText = min_tens;
+    this.timer.min.innerText = min;
+    this.timer.sec_tens_tens.innerText = sec_tens;
+    this.timer.sec.innerText = sec;
   }
 }
 
